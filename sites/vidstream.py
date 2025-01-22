5bca68dcd144e959b439ab440ef15e52578e9c40
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-01-22 14:54:07 +0530

    Add files via upload

--- sites/vidstream.py
+++ sites/vidstream.py
@@ -1,108 +1,85 @@
 import requests
-import re
-import os
 from bs4 import BeautifulSoup
-from pathlib import Path
-import base64
-import hashlib
-from Crypto.Cipher import AES
-from Crypto.Hash import SHA256
-from Crypto.Util.Padding import unpad
+import os
+from urllib.parse import urlparse, unquote
+import re
 import json
-from js2py import EvalJs
-
+import base64
+import zlib
 
 class Colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    header = '\033[95m'
+    okblue = '\033[94m'
+    okcyan = '\033[96m'
+    okgreen = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[91m'
+    endc = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
 
-
-def save_file_in_internal_directory(filename, content, directory='/storage/emulated/0'):
-    file_path = os.path.join(directory, filename)
-    try:
-        with open(file_path, 'w') as file:
-            file.write(content)
-            print(f"File saved to {file_path}")
-    except Exception as e:
-        print(f"Error saving file: {e}")
-
-
-def pad_key(key):
-    # Use SHA-256 to ensure key is 16 bytes long (AES-128)
-    hash_obj = SHA256.new()
-    hash_obj.update(key.encode('utf-8'))
-    return hash_obj.digest()[:16]
-
-
+base_url = "https://vidstreamnew.xyz/v/EDMfWZnXmaYU/"
 default_domain = "https://vidstreamnew.xyz/"
-print(f"\n{Colors.OKCYAN}TARGET: {default_domain} {Colors.ENDC}")
-session = requests.Session()
-
-initial_headers = {
-    "Referer": default_domain,
-    "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
+request_headers = {
+    'Referer': default_domain,
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 11; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36'
 }
 
-base_url = "https://vidstreamnew.xyz/v/84oPWpVecrIB/"
-initial_response = requests.get(base_url, headers=initial_headers)
-initial_page_html = initial_response.text
+print(f"\n{Colors.okcyan}TARGET: {default_domain}{Colors.endc}")
 
-pattern = r'JScripts = \'(.*)\''
-matcher = re.search(pattern, initial_page_html)
+http_session = requests.Session()
 
-if matcher:
-    json_data = json.loads(matcher.group(1))
+initial_page_content = http_session.get(base_url, headers=request_headers).text
 
-    ct = base64.b64decode(json_data['ct'])
-    iv = bytes.fromhex(json_data['iv'])
-    salt = bytes.fromhex(json_data['s'])
+encrypted_data_pattern = r"const\s+Encrypted\s*=\s*'(.*?)'"
+encrypted_data_match = re.search(encrypted_data_pattern, initial_page_content)
 
-    assert len(iv) == 16, "IV must be 16 bytes long"
-
-    pass_phrase = b"KB3c1lgTx6cHL3W"
-    print(f'\n\n{Colors.OKCYAN}Using Password: {pass_phrase}{Colors.ENDC}')
-
-    md = hashlib.md5()
-    md.update(pass_phrase)
-    md.update(salt)
-    cache0 = md.digest()
-
-    md = hashlib.md5()
-    md.update(cache0)
-    md.update(pass_phrase)
-    md.update(salt)
-    cache1 = md.digest()
-    key = cache0 + cache1
-
-    assert len(ct) % 16 == 0, "Ciphertext length must be a multiple of 16 bytes"
-
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    result = cipher.decrypt(ct)
-
-    try:
-        decrypted_plaintext = unpad(result, AES.block_size)
-        javascript = json.loads(decrypted_plaintext)
-        pattern = r'\"file\":\"(https?:\/\/[^\"]+)\"'
-        matcher = re.search(pattern, javascript)
-        initial_response = requests.get(matcher.group(1), headers=initial_headers)
-        print(initial_response.text)
-        if matcher:
-            print("######################")
-            print("######################")
-            print(f"Captured URL: {Colors.OKGREEN}{matcher.group(1)}{Colors.ENDC}")
-            print("######################")
-            print("######################\n")
-            print(f"\n{Colors.WARNING}###Please use header Referer: {default_domain} and a valid User-Agent to access the url")
-        else:
-            print("No matching file URL found in decrypted JavaScript.")
-    except ValueError as e:
-        print("Error unpadding the decrypted plaintext:", e)
+encrypted_data = ""
+if encrypted_data_match:
+    encrypted_data = encrypted_data_match.group(1)
 else:
-    print("Pattern not found in initial HTML.")
+    print("No encrypted data found.")
+
+decoded_bytes = base64.b64decode(encrypted_data)
+decoded_characters = []
+
+for byte in decoded_bytes:
+    binary_representation = f'{byte:08b}'
+    reversed_binary = binary_representation[::-1]
+    reversed_integer = int(reversed_binary, 2)
+    decoded_characters.append(reversed_integer)
+
+byte_array = bytearray(decoded_characters)
+decompressed_data = ""
+try:
+    decompressed_data = zlib.decompress(byte_array).decode('utf-8')
+except zlib.error:
+    print("The data is not a valid ZLIB compressed file.")
+except Exception as e:
+    print(f"An error occurred during decompression: {e}")
+
+special_to_alphabet_map = {
+    "!": "a", "@": "b", "#": "c", "$": "d", "%": "e",
+    "^": "f", "&": "g", "*": "h", "(": "i", ")": "j"
+}
+
+processed_data = ''.join(special_to_alphabet_map.get(char, char) for char in decompressed_data)
+decoded_base64_data = base64.b64decode(processed_data)
+
+decoded_url = unquote(decoded_base64_data.decode('utf-8'))
+
+video_url_pattern = r'file:\s*"([^"]+)"'
+video_url_match = re.search(video_url_pattern, decoded_url)
+
+video_url = ""
+if video_url_match:
+    video_url = video_url_match.group(1)
+else:
+    print("No video URL found.")
+
+print("######################")
+print("######################")
+print(f"Captured URL: {Colors.okgreen}{video_url}{Colors.endc}")
+print("######################")
+print("######################")
+print(f"{Colors.warning}### Please use the header \"Referer: https://vidstreamnew.xyz\" or the CDN host to access the URL, along with a User-Agent.\n")
