017ac350219686b4d21909cf5e8b139b2e5dfd8b
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-01-30 23:02:59 +0530

    Add files via upload

--- sites/mixdrop.py
+++ sites/mixdrop.py
@@ -1,57 +1,72 @@
 import requests
 import re
-import os
-from bs4 import BeautifulSoup
-import os
-from pathlib import Path
-import base64
-from js2py import EvalJs
-import binascii
-import codecs
-import time
-import warnings
-
 
 class Colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    
-def ignore_user_warnings(message, category, filename, lineno, file=None, line=None):
-    return category == UserWarning
-warnings.showwarning = ignore_user_warnings
+    header = '\033[95m'
+    okblue = '\033[94m'
+    okcyan = '\033[96m'
+    okgreen = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[91m'
+    endc = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
 
-base_url = "https://mixdrop.ms/e/eno3x81kt94le8"
+base_url = "https://mixdrop.ps/e/q1m9d9eeuz13dx"
+user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 
-default_domain = "https://mixdrop.ms/"
-print(f"\n{Colors.OKCYAN}TARGET: {default_domain}{Colors.ENDC}")
-session = requests.Session()
-
-
-initial_headers = {
-    "Referer": base_url,
-    "User-Agent":"Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
+headers = {
+    'Referer': "https://mixdrop.ps/",
+    'User-Agent': user_agent
 }
 
-initial_response = session.get(base_url,headers=initial_headers)
-initial_page_html = initial_response.text
-pattern = 'eval\(function\(p,a,c,k,e,d\)\{.*?\}\)'
-match = re.search(pattern, initial_page_html)
-function_code = match.group(0).replace("eval(function","function hello").replace("return p}(","return p} var download_link=hello(")
-context = EvalJs()
-context.execute(function_code)
-decoded_string = context.download_link
-pattern = r'MDCore\.wurl="([^"]+)"'
-match = re.search(pattern, decoded_string)
-download_url = "https:"+match.group(1)
-print("######################")
-print("######################")
-print(f"Captured URL: {Colors.OKGREEN}{download_url}{Colors.ENDC}")
-print("######################")
-print("######################\n")
+
+# Fetch the initial response
+initial_response = requests.get(base_url, headers=headers).text
+
+pattern = r'eval\(function\((.*?)\)\{.*\}\((.*?)\)\)'
+packed_data_match = re.search(pattern, initial_response)
+
+
+# Extract packed data if found
+packed_data = ""
+if packed_data_match:
+    packed_data = packed_data_match.group(2).replace("\"", "").replace(".split('|')", "").replace("\'","").split(',')
+else:
+    print("Failed to extract packed data.")
+
+# Function to convert numbers to base-32
+def convert_base(n):
+    return str(n) if n < 10 else chr(n - 10 + ord('a'))
+
+# Extract variables from packed data
+p = packed_data[0]
+a = int(packed_data[1])
+c = int(packed_data[2])
+k = packed_data[3].split('|')
+e = int(packed_data[4])
+
+# Decrypt the data
+d = {}
+while c > 0:
+    c -= 1
+    d[convert_base(c)] = k[c] if k[c] else convert_base(c)
+
+# Replace function to decode the packed data
+final_result = re.sub(r'\b\w+\b', lambda match: d.get(match.group(0), match.group(0)), p)
+
+# Regex to find the video URL
+regex_match = re.search(r"MDCore\.wurl=([^;]+)", final_result)
+
+# Get video URL
+video_url = ""
+if regex_match:
+    video_url = f"https:{regex_match.group(1)}"
+else:
+    print(f"{Colors.fail}Video URL not found!{Colors.endc}")
+
+# Print Results
+print("\n" + "#"*25 + "\n" + "#"*25)
+print(f"Captured URL: {Colors.okgreen}{video_url}{Colors.endc}")
+print("#"*25 + "\n" + "#"*25)
+print(f"{Colors.warning}### Please use the header \"Referer: https://mixdrop.ps/\" or the CDN host to access the URL, along with a User-Agent.\n")
