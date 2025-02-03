697bdea8b42795c6cae2b65ee62d4962b17735ea
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-02-03 12:20:57 +0530

    Add files via upload

--- sites/vidstream.py
+++ sites/vidstream.py
@@ -6,14 +6,7 @@ from Crypto.Hash import SHA256
 from Crypto.Util.Padding import unpad, pad
 import struct
 
-## Library v4.5 ##
-
-'''
-Supports:
-https://vidstreamnew.xyz/
-https://moviesapi.club/
-ChillX
-'''
+## Library v4.6 ##
 
 class Colors:
     header = '\033[95m'
@@ -53,7 +46,7 @@ headers = {
 initial_response = requests.get(base_url, headers=headers).text
 
 # Extract encrypted data using regex
-encrypted_data_match = re.search(r"const\s+Matrix\s*=\s*'(.*?)'", initial_response)
+encrypted_data_match = re.search(r"const\s+Matrixs\s*=\s*'(.*?)'", initial_response)
 if not encrypted_data_match:
     print("No encrypted data found.")
     exit()
@@ -61,7 +54,7 @@ if not encrypted_data_match:
 encrypted_data = encrypted_data_match.group(1)
 
 # Decryption process
-password = "0-4_xSb3ikmo]&v%D,&7"
+password = "Fvv0O(0ep+X,q-Z+"
 
 # Decode the encrypted data
 decoded_bytes = base64.b64decode(encrypted_data)
@@ -73,12 +66,8 @@ result_words = bytes_to_32bit_words(decoded_bytes)
 iv = result_words[:4]
 iv_bytes = b''.join(word.to_bytes(4, byteorder='big', signed=True) for word in iv)
 
-# Generate a dynamic password with password and User-Agent
-
-dynamic_password = f"{password}{user_agent}"
-
 # Generate the key using SHA256 hash of the password
-key = SHA256.new(dynamic_password.encode()).digest()
+key = SHA256.new(password.encode()).digest()
 
 # Initialize the AES cipher in CBC mode
 cipher = AES.new(key, AES.MODE_CBC, iv_bytes)
@@ -105,4 +94,4 @@ else:
 print("\n" + "#"*25 + "\n" + "#"*25)
 print(f"Captured URL: {Colors.okgreen}{video_url}{Colors.endc}")
 print("#"*25 + "\n" + "#"*25)
-print(f"{Colors.warning}### Please use the header \"Referer: https://vidstreamnew.xyz\" or the CDN host to access the URL, along with the User-Agent: {Colors.okcyan}[{user_agent}]{Colors.endc}\n")
+print(f"{Colors.warning}### Please use the header \"Referer: https://vidstreamnew.xyz\" or the CDN host to access the URL, along with a User-Agent: {Colors.okcyan}[{user_agent}]{Colors.endc}\n")
