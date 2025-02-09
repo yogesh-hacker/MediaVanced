c52a6ae63bf3784f6eeae7d9b56a24e04b3565f1
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-02-09 21:23:52 +0530

    Update minoplres.py

--- sites/minoplres.py
+++ sites/minoplres.py
@@ -30,13 +30,12 @@ initial_response = get(base_url, headers = initial_headers)
 initial_page_html = initial_response.text
 pattern = r'file:"([^"]+)"'
 match = re.search(pattern, initial_page_html)
-save_file_in_internal_directory("minoplres.html", initial_page_html)
 if match:
-    stream_url = match.group(1)
+    video_url = match.group(1)
     print("\n" + "#"*25 + "\n" + "#"*25)
-    print(f"Captured URL: {Colors.OKGREEN}{stream_url}{Colors.ENDC}")
+    print(f"Captured URL: {Colors.OKGREEN}{video_url}{Colors.ENDC}")
     print("#"*25 + "\n" + "#"*25)
-    print(f"{Colors.WARNING}###Please use header Referer: https://minoplres.xyz/ or host of the CDN to access the url\n")
+    print(f"{Colors.WARNING}###Please use header Referer: https://embdproxy.xyz/ or host of the CDN to access the url\n")
     
 else:
     print("URL not found.")
