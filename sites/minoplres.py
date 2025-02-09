50b981e942ae6f888328689aebaaadc36e9e6731
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-02-09 21:22:51 +0530

    Update minoplres.py

--- sites/minoplres.py
+++ sites/minoplres.py
@@ -3,25 +3,11 @@ from bs4 import BeautifulSoup
 import re
 import os
 
-default_domain = "https://minoplres.xyz/"
-base_url = "https://minoplres.xyz/embed-xgk3dxfa15oo.html"
-initial_headers = {
-    'Referer': default_domain
-}
-
-def save_file_in_internal_directory(filename, content, directory='/storage/emulated/0'):
-    file_path = os.path.join(directory, filename)
-    try:
-        with open(file_path, 'w') as file:
-            file.write(content)
-        print(f"File saved to {file_path}")
-    except Exception as e:
-        print(f"Error saving file: {e}")
-
-proxy = {
-    "http": "http://qvgxntjn:1cyxq9jfd6rh@45.94.47.66:8110",
-    "https": "http://qvgxntjn:1cyxq9jfd6rh@45.94.47.66:8110"
-}
+'''
+Supports:
+https://minoplres.xyz/
+https://embdproxy.xyz/
+'''
 
 class Colors:
     HEADER = '\033[95m'
@@ -34,7 +20,11 @@ class Colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-print(f"\n{Colors.OKCYAN}TARGET: {default_domain}{Colors.ENDC}")
+default_domain = "https://embdproxy.xyz/"
+base_url = "https://embdproxy.xyz/embed-xgk3dxfa15oo.html"
+initial_headers = {
+    'Referer': default_domain
+}
 
 initial_response = get(base_url, headers = initial_headers)
 initial_page_html = initial_response.text
@@ -43,11 +33,9 @@ match = re.search(pattern, initial_page_html)
 save_file_in_internal_directory("minoplres.html", initial_page_html)
 if match:
     stream_url = match.group(1)
-    print("######################")
-    print("######################")
+    print("\n" + "#"*25 + "\n" + "#"*25)
     print(f"Captured URL: {Colors.OKGREEN}{stream_url}{Colors.ENDC}")
-    print("######################")
-    print("######################")
+    print("#"*25 + "\n" + "#"*25)
     print(f"{Colors.WARNING}###Please use header Referer: https://minoplres.xyz/ or host of the CDN to access the url\n")
     
 else:
