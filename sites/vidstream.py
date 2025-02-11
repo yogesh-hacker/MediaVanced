302fcdb516bd1e02a387a211ce9c2ed9823b60b2
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-02-11 20:44:22 +0530

    Fixed 403 ERROR
    Added required header to bypass 403 Error

--- sites/vidstream.py
+++ sites/vidstream.py
@@ -29,7 +29,11 @@ class Colors:
 # Constants
 base_url = "https://boosterx.stream/v/NGGJGqZKpllV/"
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
-headers = {'Referer': base_url, 'User-Agent': user_agent}
+headers = {
+    "Referer": "https://vidstreamnew.xyz",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "User-Agent": user_agent
+}
 password = "~%aRg@&H3&QEK1QV"
 
 # Fetch response
