33f4460ad212385483acefc93172c5c604438cb4
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-03-16 23:15:40 +0530

    Optimization

--- sites/saicord.py
+++ sites/saicord.py
@@ -1,53 +1,60 @@
 import requests
 import re
-import os
 from bs4 import BeautifulSoup
-import os
-from pathlib import Path
 import base64
 
 
-class Colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+'''
+Supports:
+https://saicord.com/
+'''
 
+class Colors:
+    header = '\033[95m'
+    okblue = '\033[94m'
+    okcyan = '\033[96m'
+    okgreen = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[91m'
+    endc = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+
+# Constants
+base_url = "https://saicord.com/hi/movies/1433-attack-on-finland.html"
 default_domain = "https://saicord.com/"
-print(f"\n{Colors.OKCYAN}TARGET: {default_domain}{Colors.ENDC}")
-session = requests.Session()
-initial_headers = {
+headers = {
     "Referer": default_domain,
-    "User-Agent":"Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
 }
 
 cookies = {
     "cf_clearance" : "7EMvhCoJXzLoB95qoypKRxLaPwEPPMQ_lxNTrZXUgHg-1718419969-1.0.1.1-MDMXbyRum_hdlG34Ovqctc_8sHoWFCS1PMa1xp17Rwydf_uZWwRYnKdeTUprNRxfkExtOm6K_rJPxHzeKOc4KQ"
 }
 
-base_url = "https://saicord.com/hi/movies/1433-attack-on-finland.html"
-initial_response = requests.get(base_url, headers=initial_headers, cookies=cookies)
-initial_page_html = initial_response.text
-print(initial_page_html)
-soup = BeautifulSoup(initial_page_html,"html.parser")
+# Fetch response
+initial_response = requests.get(base_url, headers=headers, cookies=cookies).text
+soup = BeautifulSoup(initial_response,"html.parser")
+
+# Get main script
 iframe = soup.find("div", attrs={"class": "player-iframe"})
 script = iframe.find_all("script")
-pattern = r'atob\("([^"]*)"\)'
-matcher = re.search(pattern, script[1].string)
-if matcher:
-    encoded_data = matcher.group(1);
-    decoded_bytes = base64.b64decode(encoded_data)
-    decoded_string = decoded_bytes.decode('utf-8')
-    pattern = r'file:"([^"]+)"'
-    matcher = re.search(pattern, decoded_string)
-    if matcher:
-        print("######################")
-        print("######################")
-        print(f"Captured URL: {Colors.OKGREEN}{matcher.group(1)}{Colors.ENDC}")
-        print("######################")
-        print("######################\n")
+
+encrypted_data_match = re.search(r'atob\("([^"]*)"\)', script[1].string)
+if not encrypted_data_match:
+    exit(print("No encrypted data found."))
+
+# Decode encrypted data
+encoded_data = encrypted_data_match.group(1);
+decoded_bytes = base64.b64decode(encoded_data)
+decoded_string = decoded_bytes.decode('utf-8')
+
+# Extract video URL
+video_match = re.search(r'file:"([^"]+)"', decoded_string)
+video_url = video_match.group(1) if video_match else exit(print("No video URL found."))
+
+# Print results
+print("\n" + "#" * 25 + "\n" + "#" * 25)
+print(f"Captured URL: {Colors.okgreen}{video_url}{Colors.endc}")
+print("#" * 25 + "\n" + "#" * 25)
+print("\n")
