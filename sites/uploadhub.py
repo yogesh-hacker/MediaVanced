e8f12738c9e03b3f3f2c36ecd018f1fcbb0ef2f8
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-03-16 22:23:56 +0530

    Optimization

--- sites/uploadhub.py
+++ sites/uploadhub.py
@@ -3,52 +3,60 @@ import re
 import os
 from bs4 import BeautifulSoup
 
-class Colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+'''
+Supports:
+https://uploadhub.wf/
+'''
 
-base_url = "http://uploadhub.wf/cadb7g83bar4"
+class Colors:
+    header = '\033[95m'
+    okblue = '\033[94m'
+    okcyan = '\033[96m'
+    okgreen = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[91m'
+    endc = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+
+base_url = "https://uploadhub.wf/0ze9m65exlci"
 
 default_domain = "https://www.uploadhub.wf/"
-print(f"\n{Colors.OKCYAN}TARGET: {default_domain}{Colors.ENDC}")
 session = requests.Session()
 
-
-initial_headers = {
+headers = {
     "Referer": base_url,
     "User-Agent":"Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
 }
 
-initial_response = session.get(base_url,headers=initial_headers)
-initial_page_html = initial_response.text
-soup = BeautifulSoup(initial_page_html,"html.parser")
+# Get first page
+initial_response = session.get(base_url, headers=headers)
+soup = BeautifulSoup(initial_response.text, "html.parser")
+
+# Get form data
 op = soup.find("input", attrs = {"name":"op"})['value']
 file_id = soup.find("input", attrs = {"name":"id"})['value']
 random = soup.find("input", attrs = {"name":"rand"})['value']
 referer = soup.find("input", attrs = {"name":"referer"})['value']
 
 payload = {
-    "op":op,
-    "adblock":"0",
-    "id":file_id,
-    "method_free":"",
-    "referer":referer,
-    "method_premium":""
+    "op": op,
+    "adblock": "0",
+    "id": file_id,
+    "method_free": "",
+    "referer": referer,
+    "method_premium": ""
 }
 
-initial_response = session.post(initial_response.url, data=payload, headers=initial_headers)
-initial_page_html = initial_response.text
-soup = BeautifulSoup(initial_page_html,"html.parser")
-download_url = soup.find("div", attrs = {"id":"direct_link"}).find("a")['href']
-print("######################")
-print("######################")
-print(f"Captured URL: {Colors.OKGREEN}{download_url}{Colors.ENDC}")
-print("######################")
-print("######################\n")
+# Post the data
+initial_response = session.post(initial_response.url, data=payload, headers=headers).text
+soup = BeautifulSoup(initial_response,"html.parser")
+
+# Extract video URL
+video_url = soup.find("div", attrs = {"id":"direct_link"}).find("a")['href']
+
+# Print results
+print("\n" + "#" * 25 + "\n" + "#" * 25)
+print(f"Captured URL: {Colors.okgreen}{video_url}{Colors.endc}")
+print("#" * 25 + "\n" + "#" * 25)
+print("\n")
