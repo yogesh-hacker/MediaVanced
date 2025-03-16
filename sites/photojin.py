e4da6fb2a93416b82870e1771874c8b010d56ca0
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-03-16 23:06:03 +0530

    Update photojin.py

--- sites/photojin.py
+++ sites/photojin.py
@@ -2,50 +2,57 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-session = requests.Session()
-initial_url = "https://photojin.lol/download/jBNOL-RbNxM"
+'''
+Supports:
+https://photojin.sbs/
+'''
 
 class Colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    header = '\033[95m'
+    okblue = '\033[94m'
+    okcyan = '\033[96m'
+    okgreen = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[91m'
+    endc = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
 
-initial_response = session.get(initial_url)
+# Constants
+base_url = "https://photojin.sbs/download/1zzzF07r1BD"
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
+headers = {
+    "Referer": "https://photojin.sbs/",
+    "User-Agent": user_agent
+}
 
-if initial_response.status_code == 200:
-    #redirected_response = session.get(initial_response)
-    soup = BeautifulSoup(initial_response.text, 'html.parser')
-    data_field = soup.find("section", id="generate_url")
-    
 
-    data = {
-        "type": "DOWNLOAD_GENERATE",
-        "payload": {
-            "uid": data_field['data-uid'],
-            "access_token": data_field['data-token']
-        }
+# Fetch response
+session = requests.Session()
+initial_response = session.get(base_url, headers=headers)
+
+
+soup = BeautifulSoup(initial_response.text, 'html.parser')
+data_field = soup.find("section", id="generate_url")
+
+# Set up post data and headers
+data = {
+    "type": "DOWNLOAD_GENERATE",
+    "payload": {
+        "uid": data_field['data-uid'],
+        "access_token": data_field['data-token']
     }
-    json_data = json.dumps(data)
+}
 
-    headers = {"X-Requested-With": "xmlhttprequest"}
-    post_response = session.post("https://photojin.online/action", data=json_data, headers=headers)
+json_data = json.dumps(data)
+headers["X-Requested-With"] = "xmlhttprequest"
 
-    captured_url = post_response.json()['download_url']
-    print("\n######################")
-    print("######################")
-    print(f"Captured URL: {Colors.OKGREEN}{captured_url}{Colors.ENDC}")
-    print("######################")
-    print("######################\n")
-else:
-    print("\n######################")
-    print("######################")
-    print(f"ERROR: Expected 200 or 302, got {initial_response.status_code}.")
-    print("######################")
-    print("######################\n")
+# Extract video URL
+initial_response = session.post("https://photojin.sbs/action", data=json_data, headers=headers)
+video_url = initial_response.json()['download_url']
 
+# Print results
+print("\n" + "#" * 25 + "\n" + "#" * 25)
+print(f"Captured URL: {Colors.okgreen}{video_url}{Colors.endc}")
+print("#" * 25 + "\n" + "#" * 25)
+print("\n")
