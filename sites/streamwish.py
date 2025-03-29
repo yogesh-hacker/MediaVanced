80e83bf9a79ac8471851c7dc4ed99a979a3d3566
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-03-30 00:21:31 +0530

    Adding New Domains & Optimization

--- sites/streamwish.py
+++ sites/streamwish.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import json
 import re
 import ast
+from urllib.parse import urlparse
 
 '''
 Supports:
@@ -16,6 +17,7 @@ https://multimovies.cloud/
 https://hlsflex.com/
 https://ghbrisk.com/
 https://niikaplayerr.com/
+https://uqloads.xyz/
 '''
 
 class Colors:
@@ -29,42 +31,50 @@ class Colors:
     bold = '\033[1m'
     underline = '\033[4m'
 
-base_url = "https://hlsflex.com/e/d2hs3drp40pw"
+base_url = "https://uqloads.xyz/e/93em7lspo9lo"
+parsed_url = urlparse(base_url)
+default_domain = f"{parsed_url.scheme}://{parsed_url.netloc}/"
 headers = {
+    'Referer': 'https://player4u.xyz/',
     'User-Agent':'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36'
 }
 
+# Utility Functions
+# Base-36 conversion helper function
+def to_base_36(n):
+    return '' if n == 0 else to_base_36(n // 36) + "0123456789abcdefghijklmnopqrstuvwxyz"[n % 36]
 
-initial_response = requests.get(base_url, headers=headers).text
+# Replace placeholders with corresponding values
+def unpack(p,a,c,k,e,d):
+    for i in range(c):
+        if k[c - i - 1]:
+            p = re.sub(r'\b' + to_base_36(c - i - 1) + r'\b', k[c - i - 1], p)
+    return p
 
-initial_regex = r'file:"([^"]+)"'
-initial_match = re.search(initial_regex, initial_response)
+# Fetch response
+response = requests.get(base_url, headers=headers).text
 
 # Fetch and parse the initial response
-soup = BeautifulSoup(initial_response, 'html.parser')
+soup = BeautifulSoup(response, 'html.parser')
 js_code = next((script.string for script in soup.find_all('script') if script.string and "eval(function(p,a,c,k,e,d)" in script.string), "")
 
 # Extract and clean the JS code
 encoded_packed = re.sub(r"eval\(function\([^\)]*\)\{[^\}]*\}\(|.split\('\|'\)\)\)", '', js_code)
 data = ast.literal_eval(encoded_packed)
 
-# Base-36 conversion helper function
-def to_base_36(n):
-    return '' if n == 0 else to_base_36(n // 36) + "0123456789abcdefghijklmnopqrstuvwxyz"[n % 36]
+# Extract values from packed data and decode
+p,a,c,k,e,d = data[0], int(data[1]), int(data[2]), data[3].split('|'), None, None
+decoded_data = unpack(p,a,c,k,e,d)
 
-# Extract values from packed data
-p, a, c, k = data[0], int(data[1]), int(data[2]), data[3].split('|')
-
-# Replace placeholders with corresponding values
-for i in range(c):
-    if k[c - i - 1]:
-        p = re.sub(r'\b' + to_base_36(c - i - 1) + r'\b', k[c - i - 1], p)
-
-#Get Video URL
-video_url = re.search(r'file:"([^"]+)', p).group(1)
+# Get Video URL
+video_url = re.search(r'file:"([^"]+)', decoded_data).group(1)
 
 # Print Results
 print("\n" + "#"*25 + "\n" + "#"*25)
 print(f"Captured URL: {Colors.okgreen}{video_url}{Colors.endc}")
 print("#"*25 + "\n" + "#"*25)
-print(f"{Colors.warning}### Please use the header \"Referer: https://rapidplayers.com\" or the CDN host to access the URL, along with a User-Agent.\n")
+
+# Print headers by key: value
+for key, value in headers.items():
+    print(f"{Colors.okcyan}{key}:{Colors.endc} {value}")
+print("\n")
