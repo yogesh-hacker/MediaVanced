287224109955ab6d7947deede59ec5152fcf80e5
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-03-31 09:24:16 +0530

    Adding Uperbox

---
+++ sites/uperbox.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import re
+
+'''
+Supports:
+https://www.uperbox.net/
+'''
+
+class Colors:
+    header = '\033[95m'
+    okblue = '\033[94m'
+    okcyan = '\033[96m'
+    okgreen = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[91m'
+    endc = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+
+# Constants
+base_url = "https://www.uperbox.net/Z75PabgB"
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
+parsed_url = urlparse(base_url)
+default_domain = f"{parsed_url.scheme}://{parsed_url.netloc}"
+headers = {
+    "Referer": default_domain,
+    "User-Agent": user_agent
+}
+
+# Fetch Response
+response = requests.get(base_url, headers=headers).text
+
+# Get Video URL
+soup = BeautifulSoup(response, 'html.parser')
+main_container = soup.select_one('.main-container')
+
+# Get Next Page
+next_url = main_container.find('a', attrs={'class':'btn'})['href']
+response = requests.get(f"{default_domain}/{next_url}", headers=headers).text
+
+# Get video URL
+soup = BeautifulSoup(response, 'html.parser')
+download_link = soup.find('a', string=re.compile("Start Download", re.I))
+
+# # Extract video URL
+video_url = default_domain + download_link.get('href') if download_link else None
+
+# Print Results
+print("\n" + "#"*25 + "\n" + "#"*25)
+print(f"Captured URL: {Colors.okgreen}{video_url}{Colors.endc}")
+print("#"*25 + "\n" + "#"*25)
+print(f"{Colors.warning}### Use these headers to access the URL")
+
+# Print headers by key: value
+for key, value in headers.items():
+    print(f"{Colors.okcyan}{key}:{Colors.endc} {value}")
+print("\n")
