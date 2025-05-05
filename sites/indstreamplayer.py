84715591bd99b916c6b989fde59ebc808f354130
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-05-05 19:24:26 +0530

    Adding IndStreamPlayer
    Provider:- AllMovieLand

---
+++ sites/indstreamplayer.py
@@ -0,0 +1,68 @@
+import requests
+import re
+import json
+from urllib.parse import urlparse
+import sys
+
+# [TAG] IndStreamPlayer #
+
+'''
+Supports:
+https://hi351sinoom.com/
+https://jole340erun.com/
+'''
+
+class Colors:
+    header = '\033[95m'
+    okblue = '\033[94m'
+    okcyan = '\033[96m'
+    okgreen = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[91m'
+    endc = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+
+# Constants
+base_url = "https://hi351sinoom.com/play/tt13622970"
+parsed_url = urlparse(base_url)
+default_domain = f"{parsed_url.scheme}://{parsed_url.netloc}"
+
+headers = {
+    'Referer': default_domain,
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36'
+}
+
+# Get initial text response from base URL
+response = requests.get(base_url, headers=headers).text
+
+# Extract file URL from response
+match = re.search(r'"file":"([^"]+)"', response)
+if not match:
+    sys.exit("File URL not found")
+file_url = match.group(1).replace('\\/', '/')
+
+# Extract the key from response
+match = re.search(r'"key":"([^"]+)"', response)
+if not match:
+    sys.exit("Key not found")
+key = match.group(1).replace('\\/', '/')
+
+# Fetch playlist info using the extracted key
+headers['X-CSRF-TOKEN'] = key
+response = requests.post(file_url, headers=headers).json()
+
+# Fetch playlist URLs with language labels
+final_playlist = [
+    {
+        'lang': item['title'],
+        'url': requests.post(f"{default_domain}/playlist/{item['file'].replace('~', '')}.txt", headers=headers).text
+    }
+    for item in response
+]
+
+# Print results
+print("\n" + "#" * 25 + "\n" + "#" * 25)
+for playlist in final_playlist:
+    print(f'\n[{playlist['lang']}]: {Colors.okgreen}{playlist['url']}{Colors.endc}')
+print("\n" + "#" * 25 + "\n" + "#" * 25 + "\n")
