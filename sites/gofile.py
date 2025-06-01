7cb06b01f8b42e0bd3ed9f2e653cd84d62028669
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-06-02 00:46:25 +0530

    Optimized and Removed Redundant Codes

--- sites/gofile.py
+++ sites/gofile.py
@@ -2,73 +2,64 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
-guest_account_url = "https://api.gofile.io/accounts"
-base_url = "https://gofile.io/d/3YLDxX"
+'''
+Supports:
+https://gofile.io/
+'''
 
 class Colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-print(f"\n{Colors.OKCYAN}TARGET: gofile.io{Colors.ENDC}")
+    header = '\033[95m'
+    okblue = '\033[94m'
+    okcyan = '\033[96m'
+    okgreen = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[91m'
+    endc = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+
+# Constants
+base_url = "https://gofile.io/d/xjLcKn"
+guest_account_url = "https://api.gofile.io/accounts"
+headers = {
+    "Access-Control-Request-Headers": "authorization",
+    "Access-Control-Request-Method": "GET",
+    "Authorization": None
+}
 
 # Extract the file ID from the URL
 parsed_url = urlparse(base_url)
 file_id = parsed_url.path.split('/')[-1]
 
-# Fetch guest account token
+# Get guest account token
+token = None
 try:
-    guest_account_response = requests.post(guest_account_url, data={})
-    guest_account_response.raise_for_status()
-    guest_account_data = guest_account_response.json()
-    token = guest_account_data.get('data', {}).get('token')
-    if not token:
-        raise ValueError("Failed to fetch guest account token")
+    response = requests.post(guest_account_url, data={})
+    response.raise_for_status()
+    response = response.json()
+    token = response.get('data', {}).get('token')
+    headers['Authorization'] = f'Bearer {token}'
 except (requests.RequestException, ValueError) as e:
-    print("\n######################")
-    print("######################")
-    print(f"Error fetching guest account token: {e}")
-    print("######################")
-    print("######################\n")
-    exit()
+    exit(print(f"ERROR: {str(e)}"))
 
-# Prepare headers for fetching file link
-fetch_params = {"wt": "4fd6sg89d7s6"}
-query_string = requests.compat.urlencode(fetch_params)
-headers = {
-    "Access-Control-Request-Headers": "authorization",
-    "Access-Control-Request-Method": "GET",
-    "Authorization": f"Bearer {token}"
-}
+# Define the query parameters
+params = {"wt": "4fd6sg89d7s6"}
+encoded_query = requests.compat.urlencode(params)
 
-# Fetch file link
-try:
-    content_data_response = requests.get(f"https://api.gofile.io/contents/{file_id}?{query_string}", headers=headers)
-    content_data_response.raise_for_status()
-    content_data = content_data_response.json()
-    print(content_data_response.text)
-    children = content_data.get('data', {}).get('children', {})
-    if not children:
-        raise ValueError("No children found in the response data")
-    first_child_key = list(children.keys())[0]
-    link = children[first_child_key]['link']
-    print("######################")
-    print("######################")
-    print(f"Captured URL: {Colors.OKGREEN}{link}{Colors.ENDC}")
-    print("######################")
-    print("######################\n")
-    headers2 ={
-        "Cookie":"accountToken=8SmVDp6PVW1CdkUIfOl8VvLHgp1bp9nZ"
-    }
-    print(requests.get(link,headers=headers2).headers)
-except (requests.RequestException, ValueError) as e:
-    print("\n######################")
-    print("######################")
-    print(f"Error fetching file link: {e}")
-    print("######################")
-    print("######################\n")
+# Get file link
+response = requests.get(f"https://api.gofile.io/contents/{file_id}?{encoded_query}", headers=headers).json()
+children = response.get('data', {}).get('children', {})
+if not children:
+    raise ValueError("No children found in the response data")
+
+# Extract video URL
+first_child_key = list(children.keys())[0]
+video_url = children[first_child_key]['link']
+
+# Print results
+print("\n" + "#" * 25 + "\n" + "#" * 25)
+print(f"Captured URL: {Colors.okgreen}{video_url}{Colors.endc}")
+print("#" * 25 + "\n" + "#" * 25)
+print(f"{Colors.warning}### Use these headers to access the URL")
+print(f"{Colors.okcyan}Cookie: {Colors.endc}accountToken={token}")
+print("\n")
