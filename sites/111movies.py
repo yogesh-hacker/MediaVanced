155b26ec7c36fa2db76672438fe8f6a07892e514
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-06-19 00:18:57 +0530

    Fixes

--- sites/111movies.py
+++ sites/111movies.py
@@ -6,7 +6,6 @@ from Crypto.Cipher import AES
 from urllib.parse import urlparse
 from Crypto.Util.Padding import pad
 
-
 '''
 Supports:
 https://111movies.com/
@@ -32,8 +31,8 @@ default_domain = '{uri.scheme}://{uri.netloc}/'.format(uri=urlparse(base_url))
 headers = {
     "Referer": default_domain,
     "User-Agent": user_agent,
-    "X-CSRF-Token": "045NBdNVE6uLAPtsPUplzjbIkv8ZiB36",
-    "Content-Type": "application/font-woff",
+    "X-CSRF-Token": "9crboYhXqMxjX3CO6h9D9GmjD0TOyIwB",
+    "Content-Type": "application/zip",
     "X-Requested-With": "XMLHttpRequest",
 }
 
@@ -41,7 +40,7 @@ headers = {
 ''' Encodes input using Base64 with custom character mapping. '''
 def custom_encode(input):
     src = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
-    dst = "cWEZ5Y8Khgmlsjon1AIdtDaTRXSJyVBx-3pFq0zML46bCQk2_HiuG9r7UwfeNvPO"
+    dst = "MkY1udDxfeBTpO5FvV7SX0Lln9KUhm6Ci_WEawJRzG-Zo32N4s8IgtqbrcPAQjHy"
     trans = str.maketrans(src, dst)
     b64 = base64.b64encode(input.encode()).decode().replace('+', '-').replace('/', '_').replace('=', '')
     return b64.translate(trans)
@@ -56,8 +55,8 @@ if not match:
 raw_data = match.group(1)
 
 # AES encryption setup
-key_hex = "ffa2611a11208b7b56b548d18b014cacdd3613fa1a2a126f1e39992f392b9ca9"
-iv_hex = "f40786ade89618340fdd2b2ec207f41a"
+key_hex = "d32118507780dd88404e7b1ff6aa904c2d6dcb001d3b92253e4f69b4ff3ae08d"
+iv_hex = "5508bd88ea7c230d4ee31334e1125bf4"
 aes_key = bytes.fromhex(key_hex)
 aes_iv = bytes.fromhex(iv_hex)
 
@@ -66,14 +65,14 @@ padded_data = pad(raw_data.encode(), AES.block_size)
 aes_encrypted = cipher.encrypt(padded_data).hex()
 
 # XOR operation
-xor_key = bytes.fromhex("a9c9cf")
+xor_key = bytes.fromhex("df8e4b")
 xor_result = ''.join(chr(ord(char) ^ xor_key[i % len(xor_key)]) for i, char in enumerate(aes_encrypted))
 
 # Custom encoded string
 encoded_final = custom_encode(xor_result)
 
 # Make final request
-static_path = "fed2b902-62f9-5557-b812-e93750cba39d/1000081778654890/5f734b094d3295169ea5407343f85c529ac37349/acb7b5e26d91236007be3bc43d8346709bca2f621dab200df317adaa64f95267"
+static_path = "1000047336362563/9f2cc71f630612dbecee371e8cafa6d430642eec/ce/vo/APA91xX-nSpP8INTQ12SjTpw_LooWDvsc2OEna30Naw-KkIuEnGF8_--c3YTItPsUJWZ3z0Zx7L6vJVh2VnWJUxpZ-Y7jP7E8RBcBQfQFZbmAsSlMCxAH9fzLPOHGieB03hxFtIg8LviAnD4VmS9FCxIjoICHtEiCmVRXEt2QcbSfl3oKjAyyJp/35e20f11/4545f944904f8b9c8ac584a5ec6f4ecb15a4018e0d34d11e95fe27dd84b7cda1/i"
 api_servers = f"https://111movies.com/{static_path}/{encoded_final}/sr"
 response = requests.get(api_servers, headers=headers).json()
 
