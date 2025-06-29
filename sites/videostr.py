06d0be74d46bc5f685e21d6320a4140d40ac04c7
Ken <ejobatex@gmail.com>
2025-06-29 05:45:14 +0300

    update to list captions

--- sites/videostr.py
+++ sites/videostr.py
@@ -12,6 +12,7 @@ Supports:
 https://videostr.net/
 '''
 
+
 class Colors:
     header = '\033[95m'
     okblue = '\033[94m'
@@ -23,9 +24,11 @@ class Colors:
     bold = '\033[1m'
     underline = '\033[4m'
 
+
 # Constants
 provider_url = 'https://flixhq.tube/ajax/episode/sources/2521753'
-base_url = requests.get(provider_url).json()['link'] # https://videostr.net/embed-1/v2/e-1/<VIDEO_ID>?z=
+# https://videostr.net/embed-1/v2/e-1/<VIDEO_ID>?z=
+base_url = requests.get(provider_url).json()['link']
 key_url = "https://raw.githubusercontent.com/yogesh-hacker/MegacloudKeys/refs/heads/main/keys.json"
 user_agent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36"
 parsed_url = urlparse(base_url)
@@ -38,6 +41,8 @@ headers = {
 }
 
 # Utility Functions
+
+
 def openssl_key_iv(password, salt, key_len=32, iv_len=16):
     # Implements OpenSSL's EVP_BytesToKey derivation
     d = d_i = b""
@@ -45,6 +50,7 @@ def openssl_key_iv(password, salt, key_len=32, iv_len=16):
         d_i = hashlib.md5(d_i + password + salt).digest()
         d += d_i
     return d[:key_len], d[key_len:key_len + iv_len]
+
 
 def decrypt_openssl(enc_base64, password):
     data = base64.b64decode(enc_base64)
@@ -57,6 +63,20 @@ def decrypt_openssl(enc_base64, password):
     padding_length = decrypted[-1]
     return decrypted[:-padding_length].decode()
 
+# Extract captions
+
+
+def print_captions(tracks):
+    captions = []
+    if tracks:
+        for track in tracks:
+            if track.get('kind') == 'captions':
+                label = track.get('label', 'Unknown')
+                url = track.get('file', 'URL not available')
+                captions.append({'label': label, 'url': url})
+    return captions
+
+
 # Fetch initial response
 response = requests.get(base_url, headers=headers).text
 soup = BeautifulSoup(response, 'html.parser')
@@ -68,10 +88,16 @@ if not video_tag:
 file_id = video_tag['data-id']
 
 # Get encrypted data
-response = requests.get(f'{default_domain}/embed-1/v2/e-1/getSources?id={file_id}', headers=headers).json()
+response = requests.get(
+    f'{default_domain}/embed-1/v2/e-1/getSources?id={file_id}', headers=headers).json()
 encrypted = response['sources']
 
-# Get Password 
+# Check if the response contains captions
+tracks = response['tracks'] if 'tracks' in response else None
+# Extract captions if available
+captions = print_captions(tracks)
+
+# Get Password
 response = requests.get(key_url).json()
 password = response['vidstr']
 
@@ -85,7 +111,12 @@ video_url = json_data[0]['file']
 # Print results
 print("\n" + "#" * 25 + "\n" + "#" * 25)
 print(f"Captured URL: {Colors.okgreen}{video_url}{Colors.endc}")
-print("#" * 25 + "\n" + "#" * 25)
-print(f"{Colors.warning}### Use these headers to access the URL")
+print("-" * 25)
+if captions:
+    print("\nCaptions:")
+    for caption in captions:
+        print(
+            f"- {caption['label']}: {Colors.okblue}{caption['url']}{Colors.endc}")
+print(f"\n{Colors.warning}### Use these headers to access the URL")
 print(f"{Colors.okcyan}Referer:{Colors.endc} {default_domain}")
 print("\n")
