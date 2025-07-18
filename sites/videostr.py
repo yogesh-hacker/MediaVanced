a62c25710e05973cf20423aa7dc244162fee5776
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-07-18 09:44:10 +0530

    Added Key URL [Auto Update]

--- sites/videostr.py
+++ sites/videostr.py
@@ -4,7 +4,7 @@ import base64
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, quote_plus
-import os
+
 
 '''
 Supports:
@@ -27,7 +27,7 @@ provider_url = 'https://flixhq.tube/ajax/episode/sources/11998768'
 base_url = requests.get(provider_url).json()['link'] # https://videostr.net/embed-1/v3/e-1/<VIDEO_ID>?z=
 user_agent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36"
 decode_url ="https://script.google.com/macros/s/AKfycbwSUvTtQrYJlFQUvp143oKp_C4iua0sw2SiMIb2Xa5az2I647_yHxlqnsc19qUSts6Zpw/exec"
-key = "AaND3XizK1QoixkfwyJfztls3yx5LALK1XBbOgxiyolzVhE"
+key_url = "https://raw.githubusercontent.com/yogesh-hacker/MegacloudKeys/refs/heads/main/keys.json"
 parsed_url = urlparse(base_url)
 default_domain = f"{parsed_url.scheme}://{parsed_url.netloc}/"
 headers = {
@@ -47,6 +47,10 @@ if not video_tag:
     exit(print(f'{Colors.fail}Looks like URL expired!{Colors.endc}'))
 file_id = video_tag['data-id']
 nonce = re.search(r'\b[a-zA-Z0-9]{48}\b', response).group()
+
+# Get Passphrase 
+response = requests.get(key_url).json()
+key = response['vidstr']
 
 # Get encrypted data
 response = requests.get(f'{default_domain}/embed-1/v3/e-1/getSources?id={file_id}&_k={nonce}', headers=headers).json()
