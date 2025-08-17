fe893547ec32329c770e6bec3f4717612c3a493c
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-08-17 19:38:04 +0530

    Fixed for Latest ✅

--- sites/vidfast.py
+++ sites/vidfast.py
@@ -33,8 +33,7 @@ headers = {
     "Accept": "*/*",
     "Referer": default_domain,
     "User-Agent": user_agent,
-    "Content-Type": "multipart/form-data",
-    "X-Csrf-Token": "amvND17blbPdx4A4UPzVymTtNIvYEh0Z",
+    "X-Requested-With": "XMLHttpRequest",
     "x-session": "",
 }
 
@@ -42,7 +41,7 @@ headers = {
 ''' Encodes input using Base64 with custom character mapping. '''
 def custom_encode(input_bytes):
     source_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
-    target_chars = "dAyC1IE4qPhDbU7a9VZNvmHuxMcJlWRGK-OnF5306zsS2Lt8BXY_wpkrTQeigofj"
+    target_chars = "y47kSpo3zvebCJlR6_EFTgNm92qsDu5hjwrZYUWdci8tBIOXL01KVafQMxHAP-Gn"
     translation_table = str.maketrans(source_chars, target_chars)
     encoded = base64.urlsafe_b64encode(input_bytes).decode().rstrip('=')
     return encoded.translate(translation_table)
@@ -57,8 +56,8 @@ if not match:
 raw_data = match.group(1)
 
 # AES encryption setup
-key_hex = '4d11cd7be582838bc6167516efb4a3d17da0ac3b8ff25747356e2bb082136451'
-iv_hex = '74829be457d1d490e2177e3858323055'
+key_hex = '02161158487ea0c39d9adc615c340fe9a9c5ed45a7f28b30287677a1c83650bc'
+iv_hex = '119600b676f4bfae69738a4fb41fb1f3'
 aes_key = bytes.fromhex(key_hex)
 aes_iv = bytes.fromhex(iv_hex)
 
@@ -68,22 +67,22 @@ padded_data = pad(raw_data.encode(), AES.block_size)
 aes_encrypted = cipher.encrypt(padded_data)
 
 # XOR operation
-xor_key = bytes.fromhex("5174616132efa590ce")
+xor_key = bytes.fromhex("19985f")
 xor_result = bytes(b ^ xor_key[i % len(xor_key)] for i, b in enumerate(aes_encrypted))
 
 # Encode XORed data
 encoded_final = custom_encode(xor_result)
 
 # Get streaming servers
-static_path = "hezushon/77eb36ec-31f1-5fcc-88d6-12607b000ac5/se/1000001989633574/j/APA91sPzN24znjMAH-E9Npu_c4ozYS8iKmDkbm6M6LFLHbcRhq0DQNujZeoj5linVlP749fMh4OnwAm1j-b_8KqzukCxA7uAbOFvP9El-L_Hh1wmwwJ9wjkZulgX2WX44CCRFgJ3j1ZhEx__Z6vo-5bn9BbHsA5favUa5KuZhVsiB9S_QPMg7rq"
+static_path = "hezushon/17ebc1e1-bb78-5c47-b057-75b917c1bf21/doitor/1000016751300953"
 data = {}
-api_servers = f"https://vidfast.pro/{static_path}/x3B5/{encoded_final}"
-response = requests.post(api_servers, headers=headers, json=data).json()
+api_servers = f"https://vidfast.pro/{static_path}/YGLuLsd-KA/{encoded_final}"
+response = requests.get(api_servers, headers=headers, json=data).json()
 
 # Select a random server
 server = random.choice(response)['data']
-api_stream = f"https://vidfast.pro/{static_path}/Mqa2mQ/{server}"
-response = requests.post(api_stream, headers=headers).json()
+api_stream = f"https://vidfast.pro/{static_path}/7ip0-iHQu_w/{server}"
+response = requests.get(api_stream, headers=headers).json()
 
 # Extract video URL
 video_url = response['url']
