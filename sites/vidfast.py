e78707d99cded880251cba69f1a73c0b78fad865
Arseniy D. <arseniydubovkin@gmail.com>
2025-09-02 15:38:51 -0700

    Update keys and encoding for vidfast extractor

--- sites/vidfast.py
+++ sites/vidfast.py
@@ -34,7 +34,6 @@ headers = {
     "Accept": "*/*",
     "Referer": default_domain,
     "User-Agent": user_agent,
-    "X-Csrf-Token": "YR9zR6b2UExb9AoD1jabbn9LnPAaWqZQ",
     "X-Requested-With": "XMLHttpRequest",
 }
 
@@ -42,7 +41,7 @@ headers = {
 ''' Encodes input using Base64 with custom character mapping. '''
 def custom_encode(input_bytes):
     source_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
-    target_chars = "X4z1J0oY9QB3ax7brfpMvVk_WesGSDKHOLhZ2qij-NtCP85duEIyAgwFnTURcl6m"
+    target_chars = "19aQPL7DwphTK_xe0W8OUdRfE4lA-coSGjuzbN5q3BHtvrmXVMFkCsiIgy6Yn2JZ"
     translation_table = str.maketrans(source_chars, target_chars)
     encoded = base64.urlsafe_b64encode(input_bytes).decode().rstrip('=')
     return encoded.translate(translation_table)
@@ -60,8 +59,8 @@ if not match:
 raw_data = match.group(1)
 
 # AES encryption setup
-key_hex = 'd43b11390b36ec7f6c7987c147498e7f897e41b622655c700b4086e8ca8e44d4'
-iv_hex = 'f317d417858b415b75385714fce28ac3'
+key_hex = '9c746a308f48f785fbe57488a6669e6d2f366f40e7c80101b24a264404e7ef9f'
+iv_hex = '73ef6dd6f01c75f22566198b4d9ef6b9'
 aes_key = bytes.fromhex(key_hex)
 aes_iv = bytes.fromhex(iv_hex)
 
@@ -71,21 +70,21 @@ padded_data = pad(raw_data.encode(), AES.block_size)
 aes_encrypted = cipher.encrypt(padded_data)
 
 # XOR operation
-xor_key = bytes.fromhex("476a3dcf7a4e0d96f2")
+xor_key = bytes.fromhex("1a544e951d2f1545ca")
 xor_result = bytes(b ^ xor_key[i % len(xor_key)] for i, b in enumerate(aes_encrypted))
 
 # Encode XORed data
 encoded_final = custom_encode(xor_result)
 
 # Get streaming servers
-static_path = "hezushon/963678e3/bf20b0d9-7483-5ad8-9fc9-cab8917a94ee/ke/1000038450000348/e845139cb2467b2e06e62a2dc0cc8cdc541b95a0a7aff6fbf9d1a4e31524b994/c07936798352ba47715ce46c32878af7d236bdf8"
+static_path = "hezushon/e881770604510fd5f378f952c69fcb6e191d66fd436c25997dee13fcf70531da/c7f55a19115f281e0b7279f8077c3b4113084807/APA91l3tB9AWRaN-YMHbYrXpV1xNal8FWhIQqISYQGT_vQp-squv3EoJVieii9yOljS27YjQgaVwkzWEUaal6U3Hvws-64zNEDWuJ-5XsgYstm4QoGwrewlgtB6y0JueRRA7HMzj0EcyqmKggZ8c3rFTVqQ2_zQ8_p-3_B7PHcQhgJh4r2Kr91h/eno/b923c088-e851-5887-aeb5-eb973475da92/af4335c7/1000044045881926/x"
 data = {}
-api_servers = f"https://vidfast.pro/{static_path}/AjPwo0I78jA/{encoded_final}"
+api_servers = f"https://vidfast.pro/{static_path}/v54C_v8/{encoded_final}"
 response = scraper.get(api_servers).json()
 
 # Select a random server
 server = random.choice(response)['data']
-api_stream = f"https://vidfast.pro/{static_path}/b_Mb7A/{server}"
+api_stream = f"https://vidfast.pro/{static_path}/UVsx3Ss/{server}"
 response = requests.get(api_stream, headers=headers).json()
 
 # Extract video URL
