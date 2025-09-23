b77a4771bd4e4ece243aac8e328af2e3e75afc2e
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-09-23 12:01:20 +0530

    New Update on Hexa ✅

--- sites/hexa.py
+++ sites/hexa.py
@@ -13,3 +13,17 @@ is limited and not as seamless as in JavaScript.
 If you're interested, I have a working JS version  
 which you can request by contacting me on Discord.
 '''
+
+'''
+[New Update] : Hello Developers,
+
+I have completed rewriting the code for Hexa WASM. 
+However, unlike the previous version, this WASM now 
+requires a browser environment.
+I attempted to replicate it in Node.js, 
+but providing all necessary browser APIs in Node is extremely challenging.
+
+If you need access to it, I can share the code, 
+but please note that you may require a headless
+browser to create an API around it.
+'''
