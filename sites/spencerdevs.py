ad343b4ccfa7db41c30e4d2f96be545ff2b3a1ae
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-10-01 23:11:50 +0530

    Optimization ⏫✅

--- sites/spencerdevs.py
+++ sites/spencerdevs.py
@@ -32,73 +32,12 @@ headers = {
     'Referer': default_domain,
     'User-Agent': user_agent
 }
-charset = {
-    "00000000": "A",
-    "00000001": "B",
-    "00000010": "C",
-    "00000011": "D",
-    "00000100": "E",
-    "00000101": "F",
-    "00000110": "G",
-    "00000111": "H",
-    "00001000": "I",
-    "00001001": "J",
-    "00001010": "K",
-    "00001011": "L",
-    "00001100": "M",
-    "00001101": "N",
-    "00001110": "O",
-    "00001111": "P",
-    "00010000": "Q",
-    "00010001": "R",
-    "00010010": "S",
-    "00010011": "T",
-    "00010100": "U",
-    "00010101": "V",
-    "00010110": "W",
-    "00010111": "X",
-    "00011000": "Y",
-    "00011001": "Z",
-    "00011010": "a",
-    "00011011": "b",
-    "00011100": "c",
-    "00011101": "d",
-    "00011110": "e",
-    "00011111": "f",
-    "00100000": "g",
-    "00100001": "h",
-    "00100010": "i",
-    "00100011": "j",
-    "00100100": "k",
-    "00100101": "l",
-    "00100110": "m",
-    "00100111": "n",
-    "00101000": "o",
-    "00101001": "p",
-    "00101010": "q",
-    "00101011": "r",
-    "00101100": "s",
-    "00101101": "t",
-    "00101110": "u",
-    "00101111": "v",
-    "00110000": "w",
-    "00110001": "x",
-    "00110010": "y",
-    "00110011": "z",
-    "00110100": "0",
-    "00110101": "1",
-    "00110110": "2",
-    "00110111": "3",
-    "00111000": "4",
-    "00111001": "5",
-    "00111010": "6",
-    "00111011": "7",
-    "00111100": "8",
-    "00111101": "9",
-    "00111110": "+",
-    "00111111": "/",
-    "01000000": "="
-}
+
+# Utility Functions
+''' Binary to Base64 '''
+def binlist_to_b64(raw_binary_list):
+    b64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+    return ''.join('=' if int(b,2)==64 else b64chars[int(b,2)] for b in raw_binary_list)
 
 # Prepare API URL
 tmdb_id = base_url.split('/')[-1]
@@ -110,7 +49,7 @@ snoopdog = response.get('snoopdog')
 
 # Map binary to Base64
 raw_binary_list = re.split(r'\s+', snoopdog.strip())
-encrypted_b64 = ''.join([charset.get(binary, '?') for binary in raw_binary_list])
+encrypted_b64 = binlist_to_b64(raw_binary_list)
 encrypted_bytes = base64.b64decode(encrypted_b64)
 
 # Get encryption parameters
