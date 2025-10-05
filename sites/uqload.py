5f1e102443dcc3b6de0b27d4b6c66c7d2baacf77
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-10-05 19:32:48 +0530

    Adding New Site ➕

---
+++ sites/uqload.py
@@ -0,0 +1,52 @@
+import re
+import requests
+from urllib.parse import urlparse
+
+'''
+Supports:
+https://uqload.cx/
+'''
+
+class Colors:
+    header = '\033[95m'
+    okblue = '\033[94m'
+    okcyan = '\033[96m'
+    okgreen = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[91m'
+    endc = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+
+
+# Constants
+base_url = 'https://uqload.cx/embed-iyqt2y5sbvqk.html'
+user_agent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
+default_domain = '{uri.scheme}://{uri.netloc}/'.format(uri=urlparse(base_url))
+headers = {
+    'Referer': default_domain,
+    'User-Agent': user_agent
+}
+
+# Fetch page content
+response = requests.get(base_url, headers=headers).text
+
+# Find video URL
+match = re.search(r'sources:\s*\[\"(.*?)\"\]', response)
+if not match:
+    exit(f'{Colors.fail}Unable to locate video URL.{Colors.endc}')
+
+# Extract video URL
+
+video_url = match.group(1)
+
+# Print results
+print("\n" + "#" * 25 + "\n" + "#" * 25)
+print(f"Captured URL: {Colors.okgreen}{video_url}{Colors.endc}")
+print("#" * 25 + "\n" + "#" * 25)
+print(f"{Colors.warning}### Use these headers to access the URL")
+
+# Print headers by key: value
+for key, value in headers.items():
+    print(f"{Colors.okcyan}{key}:{Colors.endc} {value}")
+print("\n")
