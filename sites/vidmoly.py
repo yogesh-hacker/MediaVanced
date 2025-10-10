b9d2ad27fe7180cc55df066c2cbf26626e129dee
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2025-10-10 21:17:03 +0530

    Adding New Sites ➕

---
+++ sites/vidmoly.py
@@ -0,0 +1,46 @@
+import re
+import requests
+from urllib.parse import urlparse
+
+'''
+Supports:
+https://vidmoly.me/
+'''
+
+class Colors:
+    header = '\033[95m'
+    okblue = '\033[94m'
+    okcyan = '\033[96m'
+    okgreen = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[91m'
+    endc = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
+
+
+# Constants
+base_url = 'http://vidmoly.me/embed-zvojb6qxv207.html'
+user_agent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36"
+default_domain = '{uri.scheme}://{uri.netloc}/'.format(uri=urlparse(base_url))
+headers = {
+    'Referer': default_domain,
+    'User-Agent': user_agent
+}
+
+# Fetch page content
+response = requests.get(base_url, headers=headers).text
+
+# Extract video URL
+video_url = re.search(r'file:\"(.*?)\"', response).group(1)
+
+# Print results
+print("\n" + "#" * 25 + "\n" + "#" * 25)
+print(f"Captured URL: {Colors.okgreen}{video_url}{Colors.endc}")
+print("#" * 25 + "\n" + "#" * 25)
+print(f"{Colors.warning}### Use these headers to access the URL")
+
+# Print headers by key: value
+for key, value in headers.items():
+    print(f"{Colors.okcyan}{key}:{Colors.endc} {value}")
+print("\n")
