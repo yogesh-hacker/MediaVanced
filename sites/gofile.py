a708536cb280aed61706c6d6d970af6f6dea8a4c
Yogesh Kumar Jamre <58415010+yogesh-hacker@users.noreply.github.com>
2026-03-06 23:49:17 +0530

    Refactor API request handling in gofile.py
    Updated base URL, added user agent and browser language, and implemented token hashing for API requests.

--- sites/gofile.py
+++ sites/gofile.py
@@ -1,5 +1,7 @@
+import time
+import math
+import hashlib
 import requests
-from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
 '''
@@ -19,12 +21,14 @@ class Colors:
     underline = '\033[4m'
 
 # Constants
-base_url = "https://gofile.io/d/R9eH8Y"
+base_url = "https://gofile.io/d/UkC1G6"
+user_agent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36"
 guest_account_url = "https://api.gofile.io/accounts"
+browser_language = "en-GB"
+secret = "gf2026x"
 headers = {
-    "Access-Control-Request-Headers": "authorization",
-    "Access-Control-Request-Method": "GET",
-    "Authorization": None
+    "Referer": "https://gofile.io/",
+    "User-Agent": user_agent
 }
 
 # Extract the file ID from the URL
@@ -42,12 +46,20 @@ try:
 except (requests.RequestException, ValueError) as e:
     exit(print(f"ERROR: {str(e)}"))
 
-# Define the query parameters
-params = {"wt": "4fd6sg89d7s6"}
-encoded_query = requests.compat.urlencode(params)
+# Current 4-hour interval
+interval = str(math.floor(time.time() / 14400))
+
+# Build message and hash it
+message = "::".join([user_agent, browser_language, token, interval, secret])
+hashed_token = hashlib.sha256(message.encode('utf-8')).hexdigest()
+
+# Set Hash as Token
+headers["X-BL"] = browser_language
+headers["X-Website-Token"] = hashed_token
+
 
 # Get file link
-response = requests.get(f"https://api.gofile.io/contents/{file_id}?{encoded_query}", headers=headers).json()
+response = requests.get(f"https://api.gofile.io/contents/{file_id}?contentFilter=&page=1&pageSize=1000&sortField=name&sortDirection=1", headers=headers).json()
 children = response.get('data', {}).get('children', {})
 if not children:
     raise ValueError("No children found in the response data")
